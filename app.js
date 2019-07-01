d9ceb2425c3c03e51f6cda6c08cc1a42ab2dd047
Lisonallie <allisonvanlinden@gmail.com>
2019-07-01 15:44:04 +0200

    set up working API function

--- app.js
+++ app.js
@@ -1,11 +1,11 @@
 const key = "00c6cccdf21220d91f2568a81e29690e";
-const submit = document.getElementById("button");
+const button = document.getElementById("button");
 const input = document.getElementById("input");
 
 function getWeather() {
-    submit[0].addEventListener("keypress", async function() {
+    button.addEventListener("click", async function() {
         let city = input.value;
-        let api = `http://api.openweathermap.org/data/2.5/forecast?q=${city}&mode=json&APPID=${key}`;
+        let api = `http://api.openweathermap.org/data/2.5/weather?q=${city}&mode=json&APPID=${key}`;
         let response = await axios.get(api);
         console.log(response.data);
     });
