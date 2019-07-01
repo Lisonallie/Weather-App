1500136946966f1401e899067d3da0bd3f8742d9
Lisonallie <allisonvanlinden@gmail.com>
2019-07-01 15:24:03 +0200

    set up async function

--- app.js
+++ app.js
@@ -2,3 +2,8 @@ const key = "00c6cccdf21220d91f2568a81e29690e";
 const submit = document.getElementById("button");
 const input = document.getElementById("input");
 
+function getWeather() {
+    submit[0].addEventListener("keypress", async function() {
+        
+    })
+}
