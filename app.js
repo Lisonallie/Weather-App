e7687e36ec7e1433a2995a61e0da6caa1cb4a23f
Lisonallie <allisonvanlinden@gmail.com>
2019-07-02 14:04:59 +0200

    add array set up for week

--- app.js
+++ app.js
@@ -1,19 +1,32 @@
 const key = "00c6cccdf21220d91f2568a81e29690e";
 const input = document.getElementById("input");
+let container = document.getElementById("weather-container");
+let div = document.createElement('div');
+let dayOne = [];
+let dayTwo = [];
+let dayThree = [];
+let dayFour = [];
+let dayFive = [];
+let week = [];
 
-input.addEventListener("keypress", function(e) {
+
+input.addEventListener("keypress", function (e) {
     if (e.keyCode == 13 || e.which == 13) {
         getWeather();
     }
 });
 
-function openWeather() {
-    
-}
 
 async function getWeather() {
-        let city = input.value;
-        let api = `http://api.openweathermap.org/data/2.5/forecast?q=${city}&mode=json&APPID=${key}`;
-        let response = await axios.get(api);
-        console.log(response.data.list[0].main.temp)
+    let city = input.value;
+    let api = `http://api.openweathermap.org/data/2.5/forecast?q=${city}&mode=json&APPID=${key}`;
+    let response = await axios.get(api);
+
+    for (i = 0; i < response.data.list; i += 8) {
+        console.log(response.data.list[i])
+        week.push(response.data.list[i].main);
+    }
+    dayOne.push(week[0]);
+
+    // console.log(response.data.list[0].main.temp)
 }
