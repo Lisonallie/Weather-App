3b6a84af94fb66a65d6c9dbb2d551215df6d64e2
Lisonallie <allisonvanlinden@gmail.com>
2019-07-03 11:51:15 +0200

    add working code

--- app.js
+++ app.js
@@ -1,16 +1,9 @@
 const key = "00c6cccdf21220d91f2568a81e29690e";
 const input = document.getElementById("input");
 let motherBox = document.getElementById("mother-box");
-let forecast = document.querySelectorAll(".weather-box");
-let weather = [];
-let counter = 0;
-let imgIcon = document.getElementsByClassName("icon");
-// let dayOne = [];
-// let dayTwo = [];
-// let dayThree = [];
-// let dayFour = [];
-// let dayFive = [];
-// let week = [];
+let forecast = document.querySelectorAll(".weather-box > .weathertext");
+let img = document.getElementsByClassName("icon");
+let counter = 0
 
 
 input.addEventListener("keypress", function (e) {
@@ -25,31 +18,19 @@ async function getWeather() {
     let city = input.value;
     let api = `http://api.openweathermap.org/data/2.5/forecast?q=${city}&units=imperial&mode=json&APPID=${key}`;
     let response = await axios.get(api);
-    console.log(response.data.list[0])
 
-    for (i = 0; i < 5; i++) {
-        icon = response.data.list[counter].weather[0].icon
+    for (let i = 0; i < 5; i++) {
+        let icon = response.data.list[counter].weather[0].icon;
         let temp = Math.floor(response.data.list[counter].main.temp);
         let hi = Math.floor(response.data.list[counter].main.temp_max);
         let lo = Math.floor(response.data.list[counter].main.temp_min);
-        imgIcon[i].src = `http://openweathermap.org/img/wn/${icon}@2x.png`;
-
-        forecast[i].innerHTML = temp + '<br>' + hi + '<br>' + lo + '<br>';
-
-
+        let iconsUrl = `http://openweathermap.org/img/wn/${icon}@2x.png`;        
         
+        img[i].src = iconsUrl;
+        // same as    forecast[i].innerHTML = temp + '<br>' + hi + '<br>' + lo + '<br>';
+        forecast[i].innerHTML = `${temp}<br> HI: ${hi}<br> LO: ${lo}`
+        counter += 8;
+        console.log(img[i].src)
 
-        counter =+ 8;
     }
-
-
-    // for (i = 0; i < response.data.list; i += 8) {
-    //     week.push(response.data.list[i].main);
-    // }
-    // dayOne.push(week[0]);
-    // dayTwo.push(week[1]);
-    // dayThree.push(week[2]);
-    // dayFour.push(week[3]);
-    // dayFive.push(week[4]);
-
 }
