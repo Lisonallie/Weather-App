c4f4f4fa9a70d206848c01bcf3e4ba99aabf85cd
Lisonallie <allisonvanlinden@gmail.com>
2019-07-03 09:52:41 +0200

    construct first day

--- app.js
+++ app.js
@@ -23,14 +23,16 @@ async function getWeather() {
     let city = input.value;
     let api = `http://api.openweathermap.org/data/2.5/forecast?q=${city}&units=imperial&mode=json&APPID=${key}`;
     let response = await axios.get(api);
-    console.log(response.data.list)
+    console.log(response.data.list[0])
 
     for (i = 0; i < forecast.length; i++) {
-        forecast[0].innerHTML = JSON.stringify(Math.floor(response.data.list[0].main.temp));
-        forecast[1].innerHTML = JSON.stringify(Math.floor(response.data.list[8].main.temp));
-        forecast[2].innerHTML = JSON.stringify(Math.floor(response.data.list[16].main.temp));
-        forecast[3].innerHTML = JSON.stringify(Math.floor(response.data.list[24].main.temp));
-        forecast[4].innerHTML = JSON.stringify(Math.floor(response.data.list[32].main.temp));
+        let icon1 = response.data.list[0].weather[0].icon
+
+        forecast[0].innerHTML = Math.floor(response.data.list[0].main.temp) + '<br> HI: ' + Math.floor(response.data.list[0].main.temp_max) + '<br> LO: ' + Math.floor(response.data.list[0].main.temp_min) + '<br>' + new URL(`http://openweathermap.org/img/wn/${icon1}@2x.png`);
+        forecast[1].innerHTML = Math.floor(response.data.list[8].main.temp);
+        forecast[2].innerHTML = Math.floor(response.data.list[16].main.temp);
+        forecast[3].innerHTML = Math.floor(response.data.list[24].main.temp);
+        forecast[4].innerHTML = Math.floor(response.data.list[32].main.temp);
     }
 
 
